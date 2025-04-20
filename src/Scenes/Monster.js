c7fe87f7aa8dcb8b56840bbac8063343966dc7d2
Steppedcrown <steppedcrown@gmail.com>
2025-04-20 15:54:34 -0700

    Showing monster

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -4,8 +4,8 @@ class Monster extends Phaser.Scene {
         this.my = {sprite: {}};  // Create an object to hold sprite bindings
 
         //Create constants for the monster location
-        this.bodyX = 300;
-        this.bodyY = 350;
+        this.bodyX = 200;
+        this.bodyY = 200;
         
     }
 
@@ -31,7 +31,39 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        //my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+
+        // All sprite positions now relative to (0, 0) in the container
+        my.sprite.leftArm = this.add.sprite(-100, 0, "monsterParts", "arm_blueA.png");
+        my.sprite.rightArm = this.add.sprite(100, 0, "monsterParts", "arm_blueB.png");
+
+        my.sprite.leftLeg = this.add.sprite(-50, 100, "monsterParts", "leg_blueA.png");
+        my.sprite.rightLeg = this.add.sprite(50, 100, "monsterParts", "leg_blueB.png");
+
+        my.sprite.eye = this.add.sprite(-50, -100, "monsterParts", "eye_blue.png");
+        my.sprite.smile = this.add.sprite(0, -50, "monsterParts", "mouthE.png");
+        my.sprite.fangs = this.add.sprite(0, -50, "monsterParts", "mouth_closed_fangs.png");
+
+        my.sprite.antenna = this.add.sprite(0, -150, "monsterParts", "detail_blue_antenna_large.png");
+        my.sprite.ear = this.add.sprite(50, -100, "monsterParts", "detail_blue_ear_round.png");
+
+        // Create container for the monster parts
+        this.monster = this.add.container(this.bodyX, this.bodyY, [
+            my.sprite.leftArm,
+            my.sprite.rightArm,
+            my.sprite.leftLeg,
+            my.sprite.rightLeg,
+            my.sprite.eye,
+            my.sprite.smile,
+            my.sprite.fangs,
+            my.sprite.antenna,
+            my.sprite.ear
+        ]); 
+
+        // Set status of monster parts
+        my.sprite.leftArm.flipX = true;
+        my.sprite.leftLeg.flipX = true;
+        my.sprite.fangs.visible = false; 
 
         
     }
